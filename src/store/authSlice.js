81115997e5aed9bd0f6cc85ce0e96dc70bbbbc34
Chiara Arrivas <ritarrivas96@gmail.com>
2023-02-07 09:58:27 +0100

    initial commit

---
+++ src/store/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        id: null,
+    },
+    reducers: {        
+        login: (state, action) => {
+            state.id = action.payload.id
+        },
+
+        logout: (state) => {
+            state.id = null
+        }
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { login } = authSlice.actions
+
+export default authSlice.reducer
